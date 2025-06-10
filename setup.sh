7d05558f119c9225f709caa63beacdd443404bb9
mocak-tankx <mustafa@tankx.co>
2025-06-10 13:54:53 +0300

    dependency setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+git clone https://github.com/youruser/nvim-config.git ~/nvim
+rm -rf ~/.config/nvim
+ln -s ~/nvim ~/.config/nvim
+
+# Optionally install dependencies, e.g. ripgrep, fd, tree-sitter, etc.
+# sudo apt install ripgrep fd-find
+
+nvim --headless "+Lazy! sync" +qa
+
