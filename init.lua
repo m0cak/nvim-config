5f24a263b808cc7977bd0786ac016ca4f85670df
mocak-tankx <mustafa@tankx.co>
2025-07-14 14:15:58 +0300

    background fix

--- init.lua
+++ init.lua
@@ -5,11 +5,11 @@ require("copilot")
 require("debug") 
 require("autocmds")
 
-vim.cmd [[
-  highlight Normal guibg=NONE ctermbg=NONE
-  highlight NormalNC guibg=NONE ctermbg=NONE
-  highlight EndOfBuffer guibg=NONE ctermbg=NONE
-  highlight SignColumn guibg=NONE ctermbg=NONE
-  highlight LineNr guibg=NONE ctermbg=NONE
-  highlight VertSplit guibg=NONE ctermbg=NONE
-]]
+-- vim.cmd [[
+--  highlight Normal guibg=NONE ctermbg=NONE
+--  highlight NormalNC guibg=NONE ctermbg=NONE
+--  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+--  highlight SignColumn guibg=NONE ctermbg=NONE
+--  highlight LineNr guibg=NONE ctermbg=NONE
+--  highlight VertSplit guibg=NONE ctermbg=NONE
+-- ]]
